d9853ad278b58cb6459632ee73181e9dec0eacbe
queens kisivuli <queenskisivuli@gmail.com>
2023-01-07 05:50:04 +0300

    updated prittier

--- src/pages/Users/UserActivity.jsx
+++ src/pages/Users/UserActivity.jsx
@@ -1,31 +1,43 @@
-import React, {useState, useEffect} from 'react';
-import api from '../../api/api';
-import NavBar from '../../components/NavBar/NavBar';
-import UserAppointment from '../../components/UserAppointment/UserAppointment';
+import React, { useState, useEffect } from "react";
+import api from "../../api/api";
+import NavBar from "../../components/NavBar/NavBar";
+import UserAppointment from "../../components/UserAppointment/UserAppointment";
 
-export default function UserActivity({authenticated, userName, userId, setAuthenticated}) {
+export default function UserActivity({
+  authenticated,
+  userName,
+  userId,
+  setAuthenticated,
+}) {
   const [userAppointment, setUserAppointment] = useState([]);
-  const token = localStorage.getItem('gateManagementApi');
-  useEffect(()=>{
-    const handleAppointmentData = () =>{
-      api.get(`/users/${userId}/`,
-      {headers: {
-          'Authorization': `Bearer ${token}`
-        }})
-      .then((response)=>{
-        setUserAppointment([...response.data.user.appointments])
-      })
-      .catch((err)=>{
-        console.log(err.response)
-      })
-    }
-    handleAppointmentData()
-  }, [token, userId])
-  
+  const token = localStorage.getItem("gateManagementApi");
+  useEffect(() => {
+    const handleAppointmentData = () => {
+      api
+        .get(`/users/${userId}/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setUserAppointment([...response.data.user.appointments]);
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    };
+    handleAppointmentData();
+  }, [token, userId]);
+
   return (
     <>
-      <NavBar authenticated={authenticated} userName={userName} userId={userId} setAuthenticated={setAuthenticated}/>
-      <UserAppointment userAppointment={userAppointment}/>
+      <NavBar
+        authenticated={authenticated}
+        userName={userName}
+        userId={userId}
+        setAuthenticated={setAuthenticated}
+      />
+      <UserAppointment userAppointment={userAppointment} />
     </>
-  )
+  );
 }

7e351b3765b2f69ccee78a8cb3d1159fcc828c16
queens kisivuli <queenskisivuli@gmail.com>
2023-01-07 05:46:25 +0300

    main app routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Users from './pages/Users/Users';
-import Admin from './pages/Admin/Admin';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Users from "./pages/Users/Users";
+import Admin from "./pages/Admin/Admin";
 
 function App() {
   return (
